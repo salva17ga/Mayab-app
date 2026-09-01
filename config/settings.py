aef61c51006d0bf6f9729de43cb4db6e5b6ddc84
Salva <salva17ga@gmail.com>
2026-09-01 17:23:14 -0600

    correct configuration for restoring password via email applied

--- config/settings.py
+++ config/settings.py
@@ -134,8 +134,8 @@ EMAIL_HOST = config('EMAIL_HOST')
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = config('EMAIL_PORT')
-EMAIL_USE_TLS = bool(config('EMAIL_USE_TLS'))
-EMAIL_USE_SSL = bool(config('EMAIL_USE_SSL'))
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
+EMAIL_USE_SSL = config('EMAIL_USE_SSL', cast=bool)
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 # tasks configuration
